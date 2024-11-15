8f94f0ac6f7ebb17f95b8da2a7aa583f4e18f47b
Maksym bc2024-5 <max.murtazin2006@gmail.com>
2024-11-15 08:02:40 +0200

    Changing the method /UploadForm.html

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ if (!fs.existsSync(cacheDir)) {
     fs.mkdirSync(cacheDir, { recursive: true });
 }
 
-// Функція для отримання шляху до файлу нотатки
+
 const getNotePath = (noteName) => path.join(cacheDir, `${noteName}.txt`);
 
 app.get('/notes/:name', (req, res) => {
@@ -41,7 +41,7 @@ app.get('/notes/:name', (req, res) => {
     res.send(noteText);
 });
 
-// PUT /notes/<ім’я нотатки>
+
 app.put('/notes/:name', (req, res) => {
     const notePath = getNotePath(req.params.name);
     if (!fs.existsSync(notePath)) {
@@ -51,7 +51,7 @@ app.put('/notes/:name', (req, res) => {
     res.send('Note updated');
 });
 
-// DELETE /notes/<ім’я нотатки>
+
 app.delete('/notes/:name', (req, res) => {
     const notePath = getNotePath(req.params.name);
     if (!fs.existsSync(notePath)) {
@@ -61,7 +61,7 @@ app.delete('/notes/:name', (req, res) => {
     res.send('Note deleted');
 });
 
-// GET /notes
+
 app.get('/notes', (req, res) => {
     const files = fs.readdirSync(cacheDir);
     const notes = files.map((file) => {
@@ -72,7 +72,7 @@ app.get('/notes', (req, res) => {
     res.json(notes);
 });
 
-// POST /write
+
 app.post('/write', (req, res) => {
     const { note_name, note } = req.body;
     const notePath = getNotePath(note_name);
@@ -83,24 +83,15 @@ app.post('/write', (req, res) => {
     res.status(201).send('Note created');
 });
 
-// GET /UploadForm.html
+
 app.get('/UploadForm.html', (req, res) => {
-    const htmlContent = `
-<html>
-   <body>
-      <h2>Upload Form</h2>
-      <form method="post" action="/write" enctype="multipart/form-data">
-         <label for="note_name_input">Note Name:</label><br>
-         <input type="text" id="note_name_input" name="note_name"><br><br>
-         <label for="note_input">Note:</label><br>
-         <textarea id="note_input" name="note" rows="4" cols="50"></textarea><br><br>
-         <button>Upload</button>
-      </form>
-      <p>If you click the "Submit" button, the form-data will be sent to a page called "/upload".</p>
-   </body>
-</html>
-  `;
-    res.send(htmlContent);
+    const formPath = path.join(cacheDir, 'UploadForm.html');
+
+    if (fs.existsSync(formPath)) {
+        res.sendFile(formPath);
+    } else {
+        res.status(404).send('UploadForm.html not found in cache directory');
+    }
 });
 
 app.listen(options.port, options.host, () => {
